d7ad65cb92362869669f8ec673cada02d41e8936
scott-nevin <scott.nevin@att.net>
2015-09-09 08:31:03 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Class/Week 2/VarOp.php
+++ Class/Week 2/VarOp.php
@@ -2,9 +2,22 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Variables and Operators</title>
+ <?php
+ /*
+ 	Scott Nevin
+	Sept 9th, 2015
+	Purpose: Investigate variables and operators
+ 
+ */
+ ?>
 </head>
 
 <body>
+<?php
+	//create variables of all primitive data types
+	
+	
+?>
 </body>
 </html>
