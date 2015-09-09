56eee7bd44a9aabf43eb98bf192b5104ef541610
scott-nevin <scott.nevin@att.net>
2015-09-09 09:21:04 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Class/Week 2/VarOp.php
+++ Class/Week 2/VarOp.php
@@ -8,7 +8,6 @@
  	Scott Nevin
 	Sept 9th, 2015
 	Purpose: Investigate variables and operators
- 
  */
  ?>
 </head>
@@ -16,8 +15,47 @@
 <body>
 <?php
 	//create variables of all primitive data types
-	$ix=(int)(9/5);
-	echo "<p> $ix </p>";
+	$ix=(int)(9/5);   //int
+	$fx=(float)(9/5); //float
+	$fy=3.12e1;       //double float scientific notation *Note* always use small e
+	$fz=3120e-2;      //double float scientific notation
+	$hx=0xFF;         //hex
+	$ox=077;          //octal
+	$bx=0b11;         //binary
+	$boolx=true;      //boolean
+	$booly=false;     //boolean
+	$sx="This is a great day!"; //String
+	$sy='This is a great day!'; //String
+	$sz='\'"This is a great day!"'; //String
+	$mx=10%3;         //Modulus operator 
+	
+	//Display results
+	echo "<p> $ix </p>"; //int
+	echo "<p> $fx </p>"; //float
+	echo "<p> $fy </p>"; //double float scientific notation *Note* always use small e
+	echo "<p> $fz </p>"; //double float scientific notation
+	echo "<p> $hx </p>"; //hex
+	echo "<p> $ox </p>"; //octal
+	echo "<p> $bx </p>"; //binary
+	echo "<p> $boolx $booly </p>"; //boolean
+	echo "<p> $sx </p>"; //String
+	echo "<p> $sy </p>"; //String
+	echo "<p> $sz </p>"; //String
+	echo '<p>"'. $sx .'"</p>'; //String cencantenation
+	echo "<p> $mx </p>"; //mod
+	
+	//Utilize the turnary operator
+	$hours=10; //hours worked **change this to test**
+	$payRate=15; //pay rate in $ per hour
+	//$payChk=$hours*$payRate; //paycheck calc normal
+	$payChk=$hours<0?0:
+		   ($hours<20?$hours*$payRate:20*$payRate+2*($hours-20)*$payRate); //if than else statement
+	//Above* if hours are less than 20, pay normal rate, else pay double time for hours above 20
+	//Structure bool ? true : false
+	//this statement chains two tests, the second is in () to separate the tests
+		
+	//output paycheck
+	echo "<p> Paycheck = $$payChk </p>";	
 	
 ?>
 </body>
