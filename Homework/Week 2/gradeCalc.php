e75df12059aacd72d8db5c2496921fa84250777c
scott-nevin <scott.nevin@att.net>
2015-09-09 17:21:37 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

---
+++ Homework/Week 2/gradeCalc.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Grade Calculation</title>
+ <?php
+ /*
+ 	Scott Nevin
+	Sept 9th, 2015
+	Purpose: Input a score and receive a grade
+ */
+ ?>
+</head>
+
+<body>
+<?php
+	//Declare variables
+	$score = 85; //Score variable **Change this to see different results**
+	$grade =$score>=90?"A":		          //if statement to test score and assign grade
+			($score<90 && $score>=80?"B":
+			($score<80 && $score>=70?"C":
+	        ($score<70 && $score>=60?"D": "F")));
+	$praise = $score>=60?"You passed!": //variable to test pass/fail
+			"Sorry, you failed.";	
+	$advice = $score>=90?"Excellent work!": 	//if statement to give advice
+			  ($score<90&&$score>=80?"Keep up the hard work!":
+			  ($score<80&&$score>=70?"Try a bit harder next time. ":
+	          ($score<70&&$score>=60?"Barely...": "You've got a lot of work to do.")));
+			
+	//Output results
+	echo "<p> Your Score: ". $score. " </p>"; //print score
+	echo "<p> Your Grade: ". $grade. " </p>"; //print grade
+	echo "<p> $praise ". $advice. " </p>";     //print possible praise and advice
+?>
+</body>
+</html>
---
+++ newProgTemplate.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Title Goes Here</title>
+ <?php
+ /*
+ 	Scott Nevin
+	created: 
+	Purpose: 
+ */
+ ?>
+</head>
+
+<body>
+<?php
+ //php code goes here
+?>
+</body>
+</html>
