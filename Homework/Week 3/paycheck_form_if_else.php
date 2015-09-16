ca1e60c51f41de333c77789f30d7a59aeca1927a
scott-nevin <scott.nevin@att.net>
2015-09-16 09:10:03 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

---
+++ Homework/Week 3/paycheck_form_if_else.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Paycheck Calculation Form</title>
+	 <?php
+	 /*
+		Scott Nevin
+		Sept 14th, 2015
+		Purpose: Form to be used with paycheck program
+	 */
+	 ?>
+	</head>
+
+	<body>
+	 <form action="paycheck_if_else.php" method="get">
+		Please enter hours worked. 
+		<input type="text" name="hours"><br>
+		Please enter rate of pay in $ per hour.
+		<input type="text" name="payRate"><br>
+		<input type="submit">
+		</form>
+	</body>
+</html>
---
+++ Homework/Week 3/paycheck_if_else.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Paycheck calculation if/else</title>
+ <?php
+ /*
+ 	Scott Nevin
+	created: 09/16/15
+	Purpose: Calculate paycheck
+ */
+ ?>
+</head>
+
+<body>
+	<?php
+		//Declare variables
+		$hours = $_GET['hours'];
+		$payRate = $_GET['payRate'];
+		$pay = 0;
+		$dblTime = $payRate * 2;
+		$trplTime = $payRate * 3;
+		
+		//test hours with if else
+		if($hours >= 40){
+			$pay += (($hours - 40) * $trplTime) + ( 20 * $dblTime) + (20 * $payRate);
+		}
+		else if($hours > 20 && $hours < 40){
+			$pay += (($hours - 20) * $trplTime) + ( 20 * $payRate);
+		}
+		else if($hours <= 20){
+			$pay += $hours * $payRate;
+		}
+		else{
+			$pay = "Invalid entries, please retry";
+		}
+		
+		//output results
+		echo "<p> Your Hours: ". $hours. " </p>";          //print hours
+		echo "<p> Your Pay Rate: $".$payRate. "/hr</p>";  //print pay rate
+		echo "<p> Your Pay: $ ". $pay. " </p>";            //print paycheck
+		
+		
+	 
+	?>
+</body>
+</html>
