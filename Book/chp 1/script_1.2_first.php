1d8f52b5282dc7efb17c73e9f15d57a8f6063f12
scott-nevin <scott.nevin@att.net>
2015-10-21 10:05:54 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

---
+++ Book/chp 1/script_1.2_first.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml/DTD/xhtml1-transitional.dtd">
+<html xmlns-"http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Page Title</title>
+</head>
+    <body>
+    	<!--Script 1.1 - template.html -->
+        <p>This is standard html.</p>
+        <?php
+		?>
+    </body>
+</html>
